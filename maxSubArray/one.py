465d82a6dbda6e8cc24c874352d67d98492a0040
Prabesh Bashyal <147370815+prabeshx12@users.noreply.github.com>
2026-08-07 23:30:13 +0545

    Leet easy (#3)
    * max sub array sum in the array using sliding window
    
    * update related to layout

---
+++ maxSubArray/one.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def maxSubArray(self, nums, window_size):
+        sub_arr = sum(nums[:window_size])
+        max_subsum = sub_arr
+        for i in range(window_size, len(nums)):
+            sub_arr += nums[i] - nums[i - window_size]
+            max_subsum = max(sub_arr, max_subsum)
+        return max_subsum
