db6fd32dbb3187bb125078e415817722cbba8f51
Prabesh <bashyalprabesh.12@gmail.com>
2026-08-07 23:27:33 +0545

    update related to layout

--- maxSubArray/one.py
+++ maxSubArray/one.py
@@ -3,6 +3,6 @@ class Solution(object):
         sub_arr = sum(nums[:window_size])
         max_subsum = sub_arr
         for i in range(window_size, len(nums)):
-            sub_arr += nums[i] - nums[i-window_size]
+            sub_arr += nums[i] - nums[i - window_size]
             max_subsum = max(sub_arr, max_subsum)
         return max_subsum
