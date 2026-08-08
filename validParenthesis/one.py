57392c2c61be7ab5f5e98a12569aa2aa4079a470
Prabesh <bashyalprabesh.12@gmail.com>
2026-08-08 08:50:35 +0545

    changes related to formatting of code making it based on Solution class

--- validParenthesis/one.py
+++ validParenthesis/one.py
@@ -1,24 +1,20 @@
-def check_valid_parenthesis(testString):
-    dictMap = {
-        ')': '(',
-        '}': '{',
-        ']': '[',
-    }
-    stack = []
+class Solution(object):
+    def check_valid_parenthesis(self, testString):
+        dictMap = {
+            ')': '(',
+            '}': '{',
+            ']': '[',
+        }
+        stack = []
 
-    for i in testString:
-        if i in '({[':
-            stack.append(i)
-        elif i in dictMap:
-            if not stack and stack[-1] != dictMap.get(i):
+        for i in testString:
+            if i in '({[':
+                stack.append(i)
+            elif i in dictMap:
+                if not stack and stack[-1] != dictMap.get(i):
+                    return False
+                stack.pop()
+            else:
                 return False
-            stack.pop()
-        else:
-            return False
 
-    return len(stack) == 0
-
-print(check_valid_parenthesis('([{[]}])'))
-print(check_valid_parenthesis('()[]'))
-print(check_valid_parenthesis('('))
-print(check_valid_parenthesis(''))
+        return len(stack) == 0
