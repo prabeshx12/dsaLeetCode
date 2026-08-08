e009be6f22e4c3086b16cdfe2269061b1a606666
Prabesh Bashyal <147370815+prabeshx12@users.noreply.github.com>
2026-08-08 08:46:52 +0545

    Two methods for reversing the string (#4)

---
+++ reverseString/one.py
@@ -0,0 +1,10 @@
+#Two pointer approach
+class Solution(object):
+    def reverseString(self, stringtext):
+        a = 0; b = len(stringtext) - 1
+        string = list(stringtext)
+        while a < b:
+            string[a], string[b] = string[b], string[a]
+            a += 1
+            b -= 1
+        return ''.join(string)
