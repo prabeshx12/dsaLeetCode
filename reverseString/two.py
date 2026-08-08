3de46e6b29fd7736200784a9f831d101b7f3693d
Prabesh <bashyalprabesh.12@gmail.com>
2026-08-08 08:45:44 +0545

    Two methods for reversing the string

---
+++ reverseString/two.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def reverseString(self, stringtext):
+        reverseNum = ''
+        for i in range(len(stringtext) - 1, -1, -1):
+            reverseNum += stringtext[i]
+
+        return reverseNum
