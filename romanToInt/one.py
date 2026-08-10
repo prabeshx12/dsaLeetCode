4018c0cd27ebd408dfc8e911d4e658f229379404
Prabesh Bashyal <147370815+prabeshx12@users.noreply.github.com>
2026-08-10 23:08:08 +0545

    Roman to int (#6)
    * Method for converting roman to int
    
    * Updated changes

---
+++ romanToInt/one.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def romanToInt(self, romanString):
+        dictMap = {
+            'I': 1, 'V': 5, 'X': 10,
+            'L': 50, 'C': 100, 'D': 500,
+            'M': 1000,
+        }
+        finalInt = 0
+        for key in range(len(romanString)):
+            # format is "if condition_that_can_be_false" and "condition that might crash"; last one is never evaluated in python
+            if key + 1 < len(romanString) and dictMap[romanString[key]] < dictMap[romanString[key + 1]]:
+                finalInt -= dictMap[romanString[key]]
+            else:
+                finalInt += dictMap[romanString[key]]
+        return finalInt
