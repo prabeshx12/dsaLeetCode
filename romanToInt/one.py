18ed8c9fe86b94bbed061788b675e6e123d2b0d6
Prabesh <bashyalprabesh.12@gmail.com>
2026-08-11 19:42:58 +0545

    Fix: indentation issues and extra spaces

--- romanToInt/one.py
+++ romanToInt/one.py
@@ -6,10 +6,12 @@ class Solution(object):
             'M': 1000,
         }
         finalInt = 0
+
         for key in range(len(romanString)):
             # format is "if condition_that_can_be_false" and "condition that might crash"; last one is never evaluated in python
             if key + 1 < len(romanString) and dictMap[romanString[key]] < dictMap[romanString[key + 1]]:
                 finalInt -= dictMap[romanString[key]]
             else:
                 finalInt += dictMap[romanString[key]]
+                
         return finalInt
