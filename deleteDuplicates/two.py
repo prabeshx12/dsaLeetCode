e556d8bf34b34862434543ebea331a1d96e8006d
Prabesh <bashyalprabesh.12@gmail.com>
2026-08-30 23:37:46 +0545

    feat: second method for deleting duplicates

---
+++ deleteDuplicates/two.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def deleteDuplicates(self, head):
+        if head is None:
+            return None
+
+        curr = head
+
+        while curr.next is not None:
+            if curr.val == curr.next.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+
+        return head
