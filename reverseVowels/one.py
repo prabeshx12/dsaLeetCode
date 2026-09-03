6625a74e27020d473a1a750a5fa0036c140766f9
Prabesh <bashyalprabesh.12@gmail.com>
2026-09-03 23:38:40 +0545

    feat: one of the method for reversing the vowels in a string

---
+++ reverseVowels/one.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        The logic is if both the pointers point towards the vowels then tuple swap them, else increase front/back
+        depending on the case of vowels.
+        """
+        vowels = "aeiou"
+        front = 0; back = len(s) - 1
+        string_list = []
+
+        for ch in s:
+            string_list.append(ch)
+
+        while front < back:
+            if string_list[front].lower() in vowels and string_list[back].lower() in vowels:
+                string_list[front], string_list[back] = string_list[back], string_list[front]
+                front += 1
+                back -= 1
+            elif string_list[front].lower() in vowels and string_list[back].lower() not in vowels:
+                back -= 1
+            elif string_list[front].lower() not in vowels and string_list[back].lower() in vowels:
+                front += 1
+            else:
+                front += 1
+                back -= 1
+
+        return ''.join(string_list)
+
+
+sol = Solution()
+print(sol.reverseVowels('leetcode'))
+        
