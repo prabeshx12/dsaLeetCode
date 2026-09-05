7896984675a77c00ab212d71ab96a82be21375a2
Prabesh <bashyalprabesh.12@gmail.com>
2026-09-06 00:02:34 +0545

    feat: changes

--- easy/addDigits/one.py
+++ easy/addDigits/one.py
@@ -1,13 +1,13 @@
 class Solution(object):
     def addDigits(self, num):
         """
-        The logic is to check the each final sum of the digits' length whether it is not 1, until that loop it
+        The logic is to check the each final sum of the digits' length whether it is not 1, until that, loop
         with the steps and when the length is 1, return the digit.
         """
         while len(str(num)) != 1:
             result = 0
             list_nums = []
-            
+
             while num != 0:
                 list_nums.append(num % 10)
                 num //= 10
