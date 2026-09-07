6f6ebe1cfe3d16e9c66042952afcd843e0e3e79e
Prabesh <bashyalprabesh.12@gmail.com>
2026-09-07 22:33:08 +0545

    update: fix docstring

--- easy/missingNumber/one.py
+++ easy/missingNumber/one.py
@@ -1,7 +1,8 @@
 class Solution(object):
     def missingNumber(self, nums):
         """
-
+        The logic is to subtract the formula of the sum n*(n+1)/2 of the range of numbers to the given
+        list sum and subtract it with the formulat one.
         """
         sum_num = 0
         for num in nums:
