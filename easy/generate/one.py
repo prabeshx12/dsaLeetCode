b2a9815ac84dc3c54711c16dad15544549bace81
Prabesh <bashyalprabesh.12@gmail.com>
2026-09-08 23:04:45 +0545

    feat: fix spacing

--- easy/generate/one.py
+++ easy/generate/one.py
@@ -11,14 +11,12 @@ class Solution:
         nCr.
         """
         list1 = []
+
         for i in range(numRows):
             list2 = []
+            
             for j in range(i + 1):
                 list2.append(int(self.is_fact(i)/(self.is_fact(j) * self.is_fact(i - j))))
             list1.append(list2)
 
         return list1
-
-
-sol = Solution()
-print(sol.generate(4))
