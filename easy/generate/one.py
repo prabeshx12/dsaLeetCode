76a4c109f6dd2ac4d0c6ae71098de1f2f12446e0
Prabesh Bashyal <147370815+prabeshx12@users.noreply.github.com>
2026-09-08 23:42:13 +0545

    Generate (#41)
    * feat: factorial method for this problem
    
    * feat: fix spacing
    
    * feat: second method without factorials and optimal one

---
+++ easy/generate/one.py
@@ -0,0 +1,22 @@
+class Solution:
+    def is_fact(self, n):
+        if n == 0:
+            return 1
+        else:
+            return n * self.is_fact(n - 1)
+
+    def generate(self, numRows):
+        """
+        The logic here is factorial driven and is using the combinations formula for the constants, i.e.
+        nCr.
+        """
+        list1 = []
+
+        for i in range(numRows):
+            list2 = []
+            
+            for j in range(i + 1):
+                list2.append(int(self.is_fact(i)/(self.is_fact(j) * self.is_fact(i - j))))
+            list1.append(list2)
+
+        return list1
