d84598fc9fd5ba5b5387c1ecf4b01214a1446a39
Prabesh <bashyalprabesh.12@gmail.com>
2026-09-12 00:12:26 +0545

    update: the method for the countBits

--- easy/countBits/one.py
+++ easy/countBits/one.py
@@ -1,16 +1,17 @@
 class Solution(object):
     def countBits(self, n):
         """
-        
+        The logic is to just make the range start from 1 to n and count each letters 1 using the
+        Brian Kernighan's Algorithm.
         """
-        list_bin = []
-        for i in range(n + 1):
+        list_bin = [0]
+        for i in range(1, n + 1):
             count = 0
-            if i == 0:
-                list_bin.append(0)
 
-            while i & (i - 1) != 0:
+            while i != 0:
                 count += 1
+                i &= (i - 1)
+                
             list_bin.append(count)
 
         return list_bin
