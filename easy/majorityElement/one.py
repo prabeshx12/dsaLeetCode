d9a9beae169cc96edb174d2a2dacdf97bf11581c
Prabesh Bashyal <147370815+prabeshx12@users.noreply.github.com>
2026-09-18 23:54:38 +0545

    update: add the missing code (#51)

--- easy/majorityElement/one.py
+++ easy/majorityElement/one.py
@@ -4,3 +4,11 @@ class Solution(object):
         The logic here is to build hashMap and check if any of the number frequency is greater than floor of
         n / 2; if so return the val else keep checking. It is not so optimal as space complexity is O(n).
         """
+        hashMap = {}
+
+        for val in nums:
+            hashMap[val] = hashMap.get(val, 0) + 1
+
+            if hashMap[val] > len(nums) // 2:
+                return val
+        
