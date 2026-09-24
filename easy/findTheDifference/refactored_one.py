042df206d772cceae5c1c871b78d753cfabc6e2c
Prabesh <bashyalprabesh.12@gmail.com>
2026-09-24 23:57:45 +0545

    update: refactored one method for the problem

--- easy/findTheDifference/refactored_one.py
+++ easy/findTheDifference/refactored_one.py
@@ -1,27 +1,23 @@
 class Solution(object):
     def findTheDifference(self, s, t):
         """
-        The logic here is to first check the base case of if either of the s or t is empty string, return the
-        non empty string. Else, we create hashMap and map the number of letter frequency. In the second loop,
+        We create hashMap and map the number of letter frequency. In the second loop,
         subtract the frequency based on the presence of the second string (t) letter matching that in the first
         string. If letter is not in the hashMap or letter frequency goes to negative then return that specific
         letter. 
         """
-        if s == "":
-            return t
-        elif t == "":
-            return s
-
         hashMap = {}
         
         for i in s:
             hashMap[i] = hashMap.get(i, 0) + 1
 
         for j in t:
-            if j in hashMap:
-                hashMap[j] -= 1
-
-                if hashMap[j] < 0:
-                    return j
-            else:
+            if j not in hashMap:
                 return j
+
+            hashMap[j] -= 1
+            
+            if hashMap[j] < 0:
+                return j
+
+        return ""
